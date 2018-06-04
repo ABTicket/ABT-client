c571f140780d68ee2af32193d40d489ce2bfb620
15331011 <540205863@qq.com>
2018-06-04 16:47:38 +0800

    commit_1

---
+++ ABT/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data () {
+        return {
+
+        }
+    },
+  }
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+body {
+  margin: 0;
+  height: 100%;
+  background: #E5E9F2;
+  overflow-y: hidden;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 100%;
+}
+</style>
